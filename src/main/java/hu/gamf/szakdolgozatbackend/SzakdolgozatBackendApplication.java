6abf44e733bbcac926a37e6cee3874898a66ee03
twead <45719524+twead@users.noreply.github.com>
2020-10-23 14:25:07 +0200

    Initial commit

---
+++ src/main/java/hu/gamf/szakdolgozatbackend/SzakdolgozatBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.gamf.szakdolgozatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzakdolgozatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzakdolgozatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/gamf/szakdolgozatbackend/SzakdolgozatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gamf.szakdolgozatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzakdolgozatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
